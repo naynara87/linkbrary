30fb9576e840a805009244a0bc4d94f3740dcce9
nayoonju <bubblenana@bubblecon.io>
2024-09-05 23:50:58 +0900

    feat: 라우터 정리

---
+++ src/Main.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { FullLayout, LandingLayout } from "./components/layout/Layout";
+function Main() {
+  return (
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route element={<LandingLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+          <Route element={<FullLayout />}>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignupPage />} />
+          </Route>
+        </Routes>
+      </App>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
