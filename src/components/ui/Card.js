981703b2389d4539d40ad4d52d426dd83444e7a0
nayoonju <bubblenana@bubblecon.io>
2024-09-11 13:37:59 +0900

    feat: card 즐겨찾기 버튼 기능 및 조회 관련 추가

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,24 +1,58 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import style from "./Card.module.scss";
 import Button from "./Button";
 import Icon from "./Icon";
-import defaultImg from "../../styles/images/links/sample.png";
+import defaultImg from "../../styles/images/links/default.png";
 import Dropdown from "./Dropdown";
+import timeAgo from "../../lib/util/timeAgo";
+import formatDate from "../../lib/util/formatDate";
+import axios from "../../lib/axios";
 
-function Card() {
+function Card({
+  id,
+  favorite,
+  url,
+  title,
+  imageSource,
+  description,
+  createdAt,
+}) {
+  const [isFavorite, setIsFavorite] = useState(favorite);
   const location = useLocation();
   const isFavoritePage = location.pathname === "/favorite";
+  const imageSrc = imageSource || defaultImg;
+
+  const handleFavoriteToggle = async () => {
+    try {
+      const newFavoriteStatus = !isFavorite;
+      await axios.put(`/links/${id}/favorite`, {
+        favorite: newFavoriteStatus,
+      });
+      setIsFavorite(newFavoriteStatus);
+    } catch (error) {
+      console.error("즐겨찾기 상태 업데이트 중 오류 발생:", error);
+    }
+  };
+  const imgError = (e) => {
+    e.target.src = defaultImg;
+  };
+
   return (
-    <div className={style.card}>
+    <div className={style.card} id={id}>
       <div className={style.cardImg}>
-        <img src={defaultImg} alt="대표 이미지" />
-        <Button className={style.button} ariaLabel="즐겨찾기 버튼">
-          <Icon name="ic_star" size="md" />
+        <img src={imageSrc} alt={title} onError={imgError} />
+        <Button
+          className={style.button}
+          aria-label="즐겨찾기 버튼"
+          onClick={handleFavoriteToggle}
+        >
+          <Icon type={isFavorite ? "star_filled" : "star"} size="md" />
         </Button>
       </div>
       <div className={style.cardInfo}>
         <div className={style.cardInfoWrap}>
-          <span className={style.time}>10 minutes ago</span>
+          <span className={style.time}>{timeAgo(createdAt)}</span>
           {!isFavoritePage && (
             <div className={style.cardEditGroup}>
               <Button type="button">
@@ -28,11 +62,8 @@ function Card() {
             </div>
           )}
         </div>
-        <p className={style.infoText}>
-          Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc
-          consequat.
-        </p>
-        <date>2023.3.15</date>
+        <p className={style.infoText}>{description}</p>
+        <span className={style.date}>{formatDate(createdAt)}</span>
       </div>
     </div>
   );
--- src/components/ui/CardGroup.js
+++ src/components/ui/CardGroup.js
@@ -1,25 +1,41 @@
 import { useLocation } from "react-router-dom";
 import style from "./CardGroup.module.scss";
 import Card from "./Card";
-import CardEditBar from "./CardEditBar";
+import FolderEditBar from "./FolderEditBar";
+import { useState, useEffect } from "react";
+import axios from "../../lib/axios";
 
-function CardListNone() {
-  return (
-    <div class="searchNone">
-      <p>저장된 링크가 없습니다</p>
-    </div>
-  );
-}
-
-function CardGroup() {
+function CardGroup({ folderId }) {
   const location = useLocation();
   const isFavoritePage = location.pathname === "/favorite";
+  const [folders, setfolders] = useState([]);
+
+  useEffect(() => {
+    async function fetchfolders() {
+      try {
+        const res = await axios.get(`/folders/${folderId}/links`);
+        if (Array.isArray(res.data.list)) {
+          setfolders(res.data.list);
+        } else {
+          console.error("Unexpected response data format:", res.data.list);
+          setfolders([]);
+        }
+      } catch (error) {
+        console.error("카드 목록 조회 중 오류 발생:", error);
+      }
+    }
+    if (folderId) {
+      fetchfolders();
+    }
+  }, [folderId]);
 
   return (
     <div className={style.cardGroup}>
-      {!isFavoritePage && <CardEditBar />}
+      {!isFavoritePage && <FolderEditBar folderId={folderId} />}
       <div className={style.cardList}>
-        <Card />
+        {folders.map((folder) => (
+          <Card key={folder.id} {...folder} />
+        ))}
       </div>
     </div>
   );
