d293a2b117c42843aa83879d767e0477ed50ad95
nayoonju <bubblenana@bubblecon.io>
2024-09-11 13:30:42 +0900

    feat: 모달 context 설정

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import LinkPage from "./pages/LinkPage";
-import FavoritePage from "./pages/FavoritePage";
-import ModalSamplePage from "./pages/ModalSamplePage";
+import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
+import SignupPage from "./pages/Signup/SignupPage";
+import LinkPage from "./pages/Link/LinkPage";
+import FavoritePage from "./pages/Favorite/FavoritePage";
+import { ModalProvider } from "./contexts/ModalProvider"; // Named import
+import Modal from "./components/ui/Modal";
 import { FullLayout, LandingLayout } from "./components/layout/Layout";
+
 function Main() {
   return (
     <BrowserRouter>
       <App>
-        <Routes>
-          <Route element={<LandingLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="link" element={<LinkPage />} />
-            <Route path="favorite" element={<FavoritePage />} />
-            <Route path="modal" element={<ModalSamplePage />} />
-          </Route>
-          <Route element={<FullLayout />}>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="signup" element={<SignupPage />} />
-          </Route>
-        </Routes>
+        <ModalProvider>
+          <Routes>
+            <Route element={<LandingLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="link" element={<LinkPage />} />
+              <Route path="favorite" element={<FavoritePage />} />
+            </Route>
+
+            <Route element={<FullLayout />}>
+              <Route path="login" element={<LoginPage />} />
+              <Route path="signup" element={<SignupPage />} />
+            </Route>
+          </Routes>
+          <Modal />
+        </ModalProvider>
       </App>
     </BrowserRouter>
   );
