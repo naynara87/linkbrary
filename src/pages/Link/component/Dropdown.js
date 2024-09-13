1267546c2f7824937df876f9c156179bbf7c948d
nayoonju <bubblenana@bubblecon.io>
2024-09-13 09:55:10 +0900

    refactor: 컴포넌트 개선

---
+++ src/pages/Link/component/Dropdown.js
@@ -0,0 +1,32 @@
+import { useCallback } from "react";
+import style from "./Dropdown.module.scss";
+import Button from "../../../components/ui/Button";
+import DeleteLinkModal from "./DeleteLinkModal";
+import EditLinkModal from "./EditLinkModal";
+import { useModal } from "../../../contexts/ModalProvider";
+
+function Dropdown({ linkId, linkUrl }) {
+  const { openModal } = useModal();
+  const handleDeleteLink = useCallback(() => {
+    openModal(<DeleteLinkModal linkId={linkId} linkUrl={linkUrl} />);
+  }, [linkId, linkUrl, openModal]);
+
+  const handleLinkEdit = useCallback(() => {
+    openModal(<EditLinkModal linkId={linkId} linkUrl={linkUrl} />);
+  }, [linkId, linkUrl, openModal]);
+
+  return (
+    <div className={style.dropdown}>
+      <ul>
+        <li>
+          <Button onClick={handleDeleteLink}>삭제하기</Button>
+        </li>
+        <li>
+          <Button onClick={handleLinkEdit}>수정하기</Button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Dropdown;
