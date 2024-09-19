47adc15c9e41b8bb5c86682e51942c2c881c4896
nayoonju <bubblenana@bubblecon.io>
2024-09-19 11:01:34 +0900

    feat: 즐겨찾기 로딩 페이지 추가

--- src/pages/Favorite/component/FavoriteGroup.js
+++ src/pages/Favorite/component/FavoriteGroup.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import style from "./FavoriteGroup.module.scss";
 import Card from "../../../components/ui/Card";
 import axios from "../../../lib/axios";
+import Loader from "../../../components/ui/Loader";
 
 function FavoriteGroup() {
   const [values, setValues] = useState([]);
+  const [loading, setLoading] = useState(false);
   async function getFavorite() {
+    setLoading(true);
     try {
       const res = await axios.get(`/favorites`);
       if (Array.isArray(res.data.list)) {
@@ -16,6 +19,8 @@ function FavoriteGroup() {
       }
     } catch (error) {
       console.error("카드 목록 조회 중 오류 발생:", error);
+    } finally {
+      setLoading(false);
     }
   }
   useEffect(() => {
@@ -24,7 +29,11 @@ function FavoriteGroup() {
 
   return (
     <div className={style.cardGroup}>
-      {values.length === 0 ? (
+      {loading ? (
+        <div className={style.searchNone}>
+          <Loader />
+        </div>
+      ) : values.length === 0 ? (
         <div className={style.searchNone}>
           <p>저장된 링크가 없습니다.</p>
         </div>
